c92b06925913317d01f92f16e23cd704a969aabb
jorge <fischer@iMac-de-Jorge.local>
2019-09-10 17:30:35 -0600

    add Register request

--- Banco BD/Service/Connection.swift
+++ Banco BD/Service/Connection.swift
@@ -10,77 +10,55 @@ import Foundation
 import Alamofire
 
 class Connection {
+    static let loggin:String = "http://18.218.255.127:3000/api/login"
+    static let register:String = "http://18.218.255.127:3000/api/register"
+    static let acount:String = "http://18.218.255.127:3000/api/createAcount"
+    static let transaction:String = "http://18.218.255.127:3000/api/transaccion"
+    
     
     static func loginGetToken(email:String,password:String){
-//        let user = LoginPost(email: email , contraseña: password)
-        
-        
-        let link = "http://18.218.255.127:3000/api/login"
+        let link = loggin
         let url = URL(string: link)!
         var request = URLRequest(url: url)
         request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
         
-        let body: HTTPHeaders = ["email" : "enrique@gmail.com",
-                                 "contraseña" : "absc1234%"]
+        let body: HTTPHeaders = ["email" : email,
+                                 "contraseña" : password]
         
         guard let bodyEncoded = try? JSONSerialization.data(withJSONObject: body, options: []) else { return }
         request.httpBody = bodyEncoded
         
         Alamofire.request(url, method: .post, parameters: body, encoding: JSONEncoding.default).responseJSON { (response) in
-            let json = response.result.value
-            
-            // Print JSON response...
-            print("*** RESPONSE (JSON): \(json)")
-            
-            
+          
             // Print the token string...
             if let data = response.data,
                 let token = try? JSONDecoder().decode(AccessToken.self, from: data) {
-                
                 print("*** TOKEN (STRING): \(token.token)")
             }
         }
+    }
+    
+    static func registerApi(nombre:String, apellido:String, email:String, contraseña:String,ConfiContra:String, fecha_Nacimiento:String){
+        let link = register
+        let url = URL(string: link)!
+        var request = URLRequest(url: url)
+        request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-type")
+        let body:HTTPHeaders = [ "nombre": nombre , "apellido": apellido, "email":email, "contraseña":contraseña,"ConfiContra":ConfiContra, "fecha_Nacimiento": fecha_Nacimiento]
         
-        //        if let request = URLRequest(url: url) {
-        //            request.httpMethod = "POST"
-        //        }
-        //        var request = URLRequest(url: url)
-        //        request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        //        request.httpMethod = "POST"
-//        do{
-//            let json = try JSONEncoder().encode(usuario)
-//            request.httpBody = json
-//            let jsonBodyString = String(data: json, encoding: .utf8)
-//            print(jsonBodyString!)
-//        }catch let errJson{
-//            print(errJson)
-//        }
-
-
-
-        
-//        let task = URLSession.shared.dataTask(with: request)
-//        { (Data, Res, Err) in
-//            if(Err != nil){
-//                print("Error: \(Err!)")
-//            }else{
-//                guard let data = Data else {return}
-//                print("DATA\n\(data)")
-//                if let responseString = String(data: data, encoding: .utf8) {
-//                    print(responseString)
-//                }
-////                do{
-////
-////                    let json = try JSONDecoder().decode(AccessToken.self, from: data)
-////
-////                    print(json.token)
-////
-////                }catch let err{
-////                    print(err)
-////                }
-//            }
-//        }
-//        task.resume()
+//        guard let bodyEncode = try? JSONSerialization.data(withJSONObject: body, options: []) else { return }
+        Alamofire.request(url, method: .post, parameters: body, encoding: JSONEncoding.default).responseJSON {(response) in
+            if let data = response.data {
+                let msg = String(data: data, encoding: .utf8)
+                print(msg!)
+                
+                
+                
+                ValidationAlert.showValidationRegister(on: Register(), msg: msg!, title: "alerta")
+                
+            }
+            
+            
+        }
     }
     
 }
